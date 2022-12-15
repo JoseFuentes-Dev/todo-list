f18181616a5b0bc3bfd6d4c690fdd5573540fe74
JoseFuentes-Dev <jocfuentes94@gmail.com>
2022-12-15 14:19:29 -0500

    correct add

--- main.js
+++ main.js
@@ -7,30 +7,34 @@ btn.onclick=function(){
 const todoInput = document.querySelector(".input");
 const todoButton = document.querySelector(".btn");
 const err = document.querySelector(".err");
-const todoList = document.querySelector(".todo-list");
+const todoList = document.querySelector(".list");
 todoButton.addEventListener("click", addTodo);
-// document.addEventListener("DOMContentLoaded", getLocalTodos);
+
 
 function addTodo(event){
     console.log(todoInput.value);
     console.log('click');
     event.preventDefault();
+
     const todoLabel = document.createElement("label");
-    const inpt = document.createElement("input");
-    inpt.setAttribute("type","checkbox");
     todoLabel.classList.add("todo");
-    inpt.classList.add("todo__state");
-    const svg = document.createElement("svg");
-    svg.classList.add("todo__icon");
-    svg.setAttribute("viewBox","0 0 200 25");
-    svg.setAttribute("xmlns","http://www.w3.org/2000/svg");
-    svg.setAttribute("xlmns:xlink","http://www.w3.org/1999/xlink");
+
+    todoLabel.innerHTML = `
+    <input class="todo__state" type="checkbox" />
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 200 25" class="todo__icon">
+      <use xlink:href="#todo__line" class="todo__line"></use>
+      <use xlink:href="#todo__box" class="todo__box"></use>
+      <use xlink:href="#todo__check" class="todo__check"></use>
+      <use xlink:href="#todo__circle" class="todo__circle"></use>
+    </svg>
+ `;
+
     const newTodo = document.createElement("div");
     newTodo.innerText = todoInput.value; 
     newTodo.classList.add("todo__text");
-    todoLabel.appendChild(inpt);
+
     todoLabel.appendChild(newTodo);
-    todoLabel.appendChild(svg);
+
     //ADDING TO LOCAL STORAGE 
     if(todoInput.value ==0){
         console.log('llene el campo')
